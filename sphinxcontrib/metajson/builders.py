76a337496640374037048b8d67c9b2635a90aa3b
cr-GeorgeReynolds1 <george.reynolds@cybereason.com>
2023-08-30 15:42:00 -0500

    dumping metadata as json converted dict

--- sphinxcontrib/metajson/builders.py
+++ sphinxcontrib/metajson/builders.py
@@ -1,5 +1,6 @@
 from sphinxcontrib.serializinghtml import JSONHTMLBuilder
 from sphinx.environment.adapters.toctree import TocTree
+import json
 
 class JSONMETAHTMLBuilder(JSONHTMLBuilder):
 
@@ -8,9 +9,10 @@ class JSONMETAHTMLBuilder(JSONHTMLBuilder):
     def get_doc_context(self, docname, body, metatags):
 
         out_dict = super(JSONMETAHTMLBuilder, self).get_doc_context(docname, body, metatags)
-
+        
         self_toctree = TocTree(self.env).get_toctree_for(docname, self, True)
         toctree = self.render_partial(self_toctree)['fragment']
         out_dict['fulltoc'] = toctree
-
+        out_dict['fullmeta'] = json.dumps(self.env.metadata)
         return out_dict
+ 
